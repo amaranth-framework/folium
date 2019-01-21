58d8b2b91dee7f804378ec59eb3c347f982d40b7
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2019-01-21 23:38:16 +0200

    changes on Response classes, renouncing Serialize class due security risks

--- src/Psr7/Response/Binn.php
+++ src/Psr7/Response/Binn.php
@@ -1,13 +1,31 @@
 <?php
+/**
+ * Copyright 2018 IT Media Connect
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+declare(strict_types=1);
 
 namespace Itmcdev\Folium\Psr7\Response;
 
-use GuzzleHttp\Psr7\Response;
-use GuzzleHttp\Psr7\Stream;
+use Zend\Diactoros\Response;
+use Zend\Diactoros\Stream;
 use use Knik\Binn\Binn;
 
-class Json extends Response
+class Binn extends Response
 {
+    const CONTENT_TYPE = 'application/binn';
+
     /**
      * Create a C++/php binn response with the given data.
      * 
@@ -27,7 +45,7 @@ class Json extends Response
         $binn = $this->binnEncode($data);
         $body = $this->createBodyFromBinn($binn);
 
-        $headers = $this->injectContentType('application/binn', $headers);
+        $headers = $this->injectContentType(self::CONTENT_TYPE, $headers);
 
         parent::__construct($body, $status, $headers);
     }
